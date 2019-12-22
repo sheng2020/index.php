452bfc7736bbca761a25be5e4cbe60faec7a1188
sheng <shp.com@163.com>
2019-12-22 06:11:19 -0500

    change it

--- test/index.php
+++ test/index.php
@@ -1,9 +1,9 @@
 <html>
  <head>
-    <title>PHP for CICD testing</title>
+    <title>this is first CICD </title>
  </head>
  <body>
   <br> Hello, this is my first app </br>
-  <br/> Server IP: <?php echo $_SERVER['SERVER_ADDR']; ?>
+ # <br/> Server IP: <?php echo $_SERVER['SERVER_ADDR']; ?>
  </body>
 </html>
