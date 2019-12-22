6253bb427d76fe5c8577f6a46fde6066869d01e8
sheng <shp.com@163.com>
2019-12-22 06:06:02 -0500

    delete the luaguage

--- test/index.php
+++ test/index.php
@@ -1,10 +1,9 @@
 <html>
-<html lang="en">
-<head>
-    <title>Hello</title>
-</head>
+ <head>
+    <title>PHP for CICD testing</title>
+ </head>
  <body>
-  <br>Hello, this is my first app </br>
+  <br> Hello, this is my first app </br>
   <br/> Server IP: <?php echo $_SERVER['SERVER_ADDR']; ?>
  </body>
 </html>
